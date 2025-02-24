b240a9dc78787d19e86331a2f8e2bc7684eb39fa
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-24 21:31:33 +0800

    updatd link

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   Meta,
   Outlet,
@@ -33,8 +34,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <div>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </div>
         {children}
         <ScrollRestoration />
