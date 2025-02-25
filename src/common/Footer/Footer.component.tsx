0e7d0ffd0edbcedb13473d6780d38eb7bd7d99cf
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-25 16:43:30 +0800

    working on footer

--- src/common/Footer/Footer.component.tsx
+++ src/common/Footer/Footer.component.tsx
@@ -11,17 +11,19 @@ export const Footer: React.FC = () => {
       sx={{
         display: 'flex',
         marginY: 2,
+        padding: 2,
         justifyContent: 'space-between',
         background: blueGrey[100],
+        color: blueGrey[500],
       }}
     >
-      <span style={{ fontSize: '12px' }}>All rights reserved vimalmenon.com </span>
-      <span style={{ fontSize: '12px' }}>
+      <span style={{ fontSize: '14px' }}>All rights reserved vimalmenon.com </span>
+      <span style={{ fontSize: '14px' }}>
         <MuiLink component={Link} href="/release-notes" underline="none" sx={{ color: 'inherit' }}>
           Release Notes
         </MuiLink>
       </span>
-      <span style={{ fontSize: '12px' }}>{version}</span>
+      <span style={{ fontSize: '14px' }}>{version}</span>
     </Box>
   );
 };
