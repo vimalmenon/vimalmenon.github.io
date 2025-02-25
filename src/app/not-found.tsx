797f55f3d990427a623770a6e1b8cec70f18a9d3
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-25 08:21:18 +0800

    created not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const Page: React.FC = () => {
+    return (
+      <div>
+        <main>
+          <div>Requested page not found</div>
+        </main>
+      </div>
+    );
+  };
+  export default Page;
+  
