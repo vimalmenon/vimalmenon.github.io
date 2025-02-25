971c11739452e59600c4809e604901dadfa9edf9
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-25 11:36:12 +0800

    create release notes

---
+++ src/data/navigation/index.ts
@@ -0,0 +1,20 @@
+const ReleaseNote = {
+  name: 'Release Notes',
+  title: 'Release Notes | Vimal Menon',
+  description: "This is Vimal Menon's personal website",
+  link: '/release-notes',
+  breadcrumbs: [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'Release Notes',
+      link: '/release-notes',
+    },
+  ],
+};
+
+export const Navigation = {
+  ReleaseNote,
+};
