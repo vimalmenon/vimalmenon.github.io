52a8ed0f53e94b9801aed822db7a24e5d6e0800d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-27 06:31:59 +0800

    updated links

--- src/common/Header/Link/Link.component.tsx
+++ src/common/Header/Link/Link.component.tsx
@@ -15,11 +15,10 @@ export const Link: React.FC<ILink> = ({ navigation }) => {
       href={navigation.link}
       underline={selected ? 'always' : 'none'}
       sx={{
-        fontSize: '16px',
         alignItems: 'center',
         display: 'flex',
         backgroundColor: selected ? teal[400] : teal[500],
-        borderRadius: 2,
+        borderRadius: 1,
         paddingX: 2,
         paddingY: 0.5,
       }}
