bbc8b56824ea9b488e7b6e6ad3a6b5a17583b601
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-02-28 06:52:56 +0800

    updated theme

--- src/data/theme/index.ts
+++ src/data/theme/index.ts
@@ -1,5 +1,11 @@
 'use client';
 
 import { createTheme } from '@mui/material/styles';
+import { teal } from '@mui/material/colors';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: teal,
+  },
+});
