5f4d7041004500e1090bea511f7ffb71b9f8eff8
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-01 10:38:34 +0800

    update config page

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -51,15 +51,26 @@ const colors = [
   blueGrey,
 ];
 
+const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 'A100', 'A200', 'A400', 'A700'];
+
 const Page: React.FC = () => {
   return (
     <Box component="main">
       <Breadcrumbs navigation={Navigation.Config} />
       <Box>Select a Theme</Box>
       <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
-        {colors.map((color) => {
+        {colors.map((color: Record<string | number, string>) => {
           return (
-            <Box sx={{ width: '100px', height: '50px', background: color[500] }} key={color[100]} />
+            <Box key={color[500]}>
+              {shades.map((shade: string | number, index) => {
+                return (
+                  <Box
+                    sx={{ width: '100px', height: '50px', background: color[shade] }}
+                    key={`${color[shade]}-${index}`}
+                  />
+                );
+              })}
+            </Box>
           );
         })}
       </Box>
