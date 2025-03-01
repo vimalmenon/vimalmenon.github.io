b42f3a0354d16df503bcff4ed09a45f7a1ca5595
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-01 20:23:19 +0800

    updated ICons

--- src/data/icons/index.ts
+++ src/data/icons/index.ts
@@ -1,9 +1,15 @@
 import CloseIcon from '@mui/icons-material/Close';
 import DoneIcon from '@mui/icons-material/Done';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import SaveIcon from '@mui/icons-material/Save';
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export const Icons = {
   Check: DoneIcon,
   Close: CloseIcon,
+  GitHub: GitHubIcon,
   Save: SaveIcon,
+  Twitter: XIcon,
+  YouTube: YouTubeIcon,
 };
