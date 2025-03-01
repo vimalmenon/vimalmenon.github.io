a278b00cbaa48acf0ae9c08752af8df5a483c430
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-01 10:26:15 +0800

    update config

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -2,17 +2,67 @@ import type { Metadata } from 'next';
 import Box from '@mui/material/Box';
 import { Breadcrumbs } from '@common';
 import { Navigation } from '@data';
+import {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+} from '@mui/material/colors';
 
 export const metadata: Metadata = {
   title: 'Config | Vimal Menon',
   description: "This is Vimal Menon's personal website",
 };
 
+const colors = [
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+];
+
 const Page: React.FC = () => {
   return (
     <Box component="main">
       <Breadcrumbs navigation={Navigation.Config} />
-      <div>This is Config page</div>
+      <Box>Select a Theme</Box>
+      <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap' }}>
+        {colors.map((color) => {
+          return (
+            <Box sx={{ width: '100px', height: '50px', background: color[500] }} key={color[100]} />
+          );
+        })}
+      </Box>
     </Box>
   );
 };
