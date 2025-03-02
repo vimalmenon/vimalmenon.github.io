77ee779f34d3aa183390e49d9ac1135beda91ce4
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-02 17:01:17 +0800

    working on mobile drawer

--- src/common/Header/Drawer/Drawer.component.tsx
+++ src/common/Header/Drawer/Drawer.component.tsx
@@ -1,24 +1,24 @@
 'use client';
 import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import { Fragment } from 'react';
 import { useAppContext } from '@context';
 import { HeaderNavigation } from '@data';
 import { Link } from '../Link';
+import { MobileDrawer } from './Drawer.style';
 
 export const Drawer: React.FC = () => {
   const { showDrawer, toggleDrawer } = useAppContext();
   return (
     <Fragment>
-      <MuiDrawer anchor={'top'} open={showDrawer} onClose={toggleDrawer}>
+      <MobileDrawer anchor={'top'} open={showDrawer} onClose={toggleDrawer}>
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, padding: 2 }}>
           {HeaderNavigation.map((nav) => (
             <Link navigation={nav} key={nav.name} />
           ))}
         </Box>
-      </MuiDrawer>
+      </MobileDrawer>
       <IconButton onClick={toggleDrawer}>
         <MenuIcon />
       </IconButton>
