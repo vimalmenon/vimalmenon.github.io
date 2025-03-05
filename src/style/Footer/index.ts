7301b4f27e61950e62095c828ff3cec39b32366c
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-05 21:54:00 +0800

    updated footer style

--- src/style/Footer/index.ts
+++ src/style/Footer/index.ts
@@ -1,11 +1,23 @@
 'use client';
 
 import { styled } from '@mui/material';
+import Box from '@mui/material/Box';
 import { grey } from '@mui/material/colors';
 
 export const StyledFooter = styled('footer')(({ theme }) => {
   return {
     background: theme.palette.mode === 'dark' ? theme.palette.background.paper : grey[100],
     display: 'flex',
+  };
+});
+
+export const StyledSocialMedia = styled(Box)(({ theme }) => {
+  return {
+    [theme.breakpoints.down('md')]: {
+      display: 'none',
+    },
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
   };
 });
