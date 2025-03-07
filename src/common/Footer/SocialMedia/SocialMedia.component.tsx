0bc6e3a1523f830b1f998f329e8f3c349fcb5252
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-07 19:33:18 +0800

    added tooltip

--- src/common/Footer/SocialMedia/SocialMedia.component.tsx
+++ src/common/Footer/SocialMedia/SocialMedia.component.tsx
@@ -1,5 +1,6 @@
 import IconButton from '@mui/material/IconButton';
 import MuiLink from '@mui/material/Link';
+import Tooltip from '@mui/material/Tooltip';
 import Link from 'next/link';
 import React from 'react';
 import { SocialMedias } from '@data';
@@ -10,17 +11,13 @@ export const SocialMedia: React.FC = () => {
     <StyledSocialMedia>
       {SocialMedias.map((media) => {
         return (
-          <MuiLink
-            component={Link}
-            href={media.link}
-            underline="none"
-            key={media.link}
-            target="_blank"
-          >
-            <IconButton>
-              <media.Icon />
-            </IconButton>
-          </MuiLink>
+          <Tooltip title={media.name} key={media.link}>
+            <MuiLink component={Link} href={media.link} underline="none" target="_blank">
+              <IconButton>
+                <media.Icon />
+              </IconButton>
+            </MuiLink>
+          </Tooltip>
         );
       })}
     </StyledSocialMedia>
