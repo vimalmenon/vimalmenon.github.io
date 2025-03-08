c0d2fa9152279427f91d91d9da66bce2fd89fbbd
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-08 16:52:26 +0800

    update headers

--- src/test/Header/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { Header } from '@common';
-
-describe('Header component', () => {
-  it('Snapshot Testing', () => {
-    const { container } = render(<Header />);
-    expect(container).toMatchSnapshot();
-  });
-});
