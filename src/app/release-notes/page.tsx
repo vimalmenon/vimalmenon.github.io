27b72a15af24c13e698b9f29924df52ad975461a
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-09 03:57:33 +0800

    upated elease notes

--- src/app/release-notes/page.tsx
+++ src/app/release-notes/page.tsx
@@ -13,15 +13,16 @@ const Page: React.FC = () => {
     <Box component="main">
       <Breadcrumbs navigation={Navigation.ReleaseNote} />
       <div>
-        <div>0.0.10</div>
+        <div>0.0.11</div>
         <ul>
-          <li>Create tag & release</li>
-          <li>Improve UI</li>
+          <li></li>
         </ul>
       </div>
       <div>
-        <div>[0.0.9 - 0.0.0]</div>
+        <div>[0.0.10 - 0.0.0]</div>
         <ul>
+          <li>Create tag & release</li>
+          <li>Download Resume</li>
           <li>[Pages] Release Notes | About</li>
           <li>Partial Google Analytics</li>
           <li>Set up CI & CD</li>
