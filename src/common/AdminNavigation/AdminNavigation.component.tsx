986cd939529e520866ba1eddbb76d66ff9fe6c30
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-11 07:01:02 +0800

    updated admin navigation

--- src/common/AdminNavigation/AdminNavigation.component.tsx
+++ src/common/AdminNavigation/AdminNavigation.component.tsx
@@ -1,10 +1,14 @@
+'use client';
+
 import { teal } from '@mui/material/colors';
 import MuiLink from '@mui/material/Link';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AdminNavigation as Navigation } from '@data';
 import { StyledAdminNavigation } from '@style';
 
 export const AdminNavigation: React.FC = () => {
+  const pathname = usePathname();
   return (
     <StyledAdminNavigation>
       {Navigation.map((data) => {
@@ -12,7 +16,7 @@ export const AdminNavigation: React.FC = () => {
           <MuiLink
             component={Link}
             href={data.link}
-            underline="always"
+            underline={pathname === data.link ? 'always' : 'none'}
             sx={{ color: teal[500], fontWeight: 'bold' }}
             key={data.name}
           >
