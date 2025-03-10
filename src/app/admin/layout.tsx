83864fd37f71eeaf2513f32f4ced822497b16849
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-11 06:23:17 +0800

    working on admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,8 +1,10 @@
 import Box from '@mui/material/Box';
+import { teal } from '@mui/material/colors';
+import MuiLink from '@mui/material/Link';
 import type { Metadata } from 'next';
-import { Fragment } from 'react';
+import Link from 'next/link';
 import { ComingSoon } from '@component';
-import { env } from '@data';
+import { AdminNavigation, env } from '@data';
 import { IReactChildren } from '@types';
 
 export const metadata: Metadata = {
@@ -13,7 +15,28 @@ export const metadata: Metadata = {
 const AdminLayout: React.FC<IReactChildren> = ({ children }) => {
   return (
     <Box>
-      {env.ENV === 'local' ? <Fragment>{children}</Fragment> : <ComingSoon page="Admin Page" />}
+      {env.ENV === 'local' ? (
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', marginY: 2 }}>
+            {AdminNavigation.map((data) => {
+              return (
+                <MuiLink
+                  component={Link}
+                  href={data.link}
+                  underline="always"
+                  sx={{ color: teal[500], fontWeight: 'bold' }}
+                  key={data.name}
+                >
+                  {data.name}
+                </MuiLink>
+              );
+            })}
+          </Box>
+          {children}
+        </Box>
+      ) : (
+        <ComingSoon page="Admin Page" />
+      )}
     </Box>
   );
 };
