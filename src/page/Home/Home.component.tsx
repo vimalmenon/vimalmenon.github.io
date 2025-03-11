8a9d2191ee63a0fe171c597bfdff835c4170bc43
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-11 22:28:55 +0800

    Added Home Page

---
+++ src/page/Home/Home.component.tsx
@@ -0,0 +1,5 @@
+import Box from '@mui/material/Box';
+
+export const Home: React.FC = () => {
+  return <Box>This is Home Pag</Box>;
+};
--- src/page/index.ts
+++ src/page/index.ts
@@ -1,2 +1,3 @@
 export * from './Admin';
 export * from './AdminConfig';
+export * from './Home';
