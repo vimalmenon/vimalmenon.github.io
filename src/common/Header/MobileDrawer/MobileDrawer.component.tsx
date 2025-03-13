078584124dfa6424c8e066570fc3de8f88d2ebd7
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-13 19:59:34 +0800

    fix mobile naivation

--- src/common/Header/MobileDrawer/MobileDrawer.component.tsx
+++ src/common/Header/MobileDrawer/MobileDrawer.component.tsx
@@ -3,11 +3,12 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import MuiLink from '@mui/material/Link';
+import MenuList from '@mui/material/MenuList';
 import { Fragment } from 'react';
 import { useAppContext } from '@context';
 import { AdminNavigation, env, HeaderNavigation, SocialMedias } from '@data';
 import { StyledMobileDrawer } from '@style';
-import { Link } from '../Link';
+import { Link } from './Link';
 
 export const MobileDrawer: React.FC = () => {
   const { showDrawer, toggleDrawer } = useAppContext();
@@ -15,15 +16,18 @@ export const MobileDrawer: React.FC = () => {
     <Fragment>
       <StyledMobileDrawer anchor={'top'} open={showDrawer} onClose={toggleDrawer}>
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, padding: 2 }}>
-          {HeaderNavigation.map((nav) => {
-            if (nav.show) {
-              return <Link navigation={nav} key={nav.name} />;
-            }
-            return null;
-          })}
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, marginLeft: 2 }}>
-            {env.IS_LOCAL && AdminNavigation.map((nav) => <Link navigation={nav} key={nav.name} />)}
-          </Box>
+          <MenuList sx={{ display: 'flex', flexDirection: 'column' }}>
+            {HeaderNavigation.map((nav) => {
+              if (nav.show) {
+                return <Link navigation={nav} key={nav.name} />;
+              }
+              return null;
+            })}
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, marginLeft: 2 }}>
+              {env.IS_LOCAL &&
+                AdminNavigation.map((nav) => <Link navigation={nav} key={nav.name} />)}
+            </Box>
+          </MenuList>
         </Box>
 
         <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-evenly' }}>
