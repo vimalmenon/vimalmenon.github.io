bdc53487d7ba15ca63bf9917263dcda14249d849
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-14 11:30:48 +0800

    added AdminWorkflows

--- src/page/AdminWorkflows/AdminWorkflows.component.tsx
+++ src/page/AdminWorkflows/AdminWorkflows.component.tsx
@@ -1,18 +1,46 @@
 'use client';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { useAdminWorkflows } from './AdminWorkflows.services';
 
 export const AdminWorkflows: React.FC = () => {
   const { llms } = useAdminWorkflows();
   return (
     <Box>
-      {llms.map((llm) => {
-        return (
-          <Box key={llm.name}>
-            {llm.name} | {llm.model} | {llm.supported ? 'True' : 'False'}
-          </Box>
-        );
-      })}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Model</TableCell>
+              <TableCell align="right">Supported</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {llms.map((llm) => {
+              return (
+                <TableRow key={llm.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell padding="normal" align="right">
+                    {llm.name}
+                  </TableCell>
+                  <TableCell padding="normal" align="right">
+                    {llm.model}
+                  </TableCell>
+                  <TableCell padding="normal" align="right">
+                    {llm.supported ? 'True' : 'False'}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
