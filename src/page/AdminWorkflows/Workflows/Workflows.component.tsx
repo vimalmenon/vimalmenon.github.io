00d2e1d6476588e57ed6f44b6a8d41b0ec25de19
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-14 12:52:59 +0800

    working on workflow

--- src/page/AdminWorkflows/Workflows/Workflows.component.tsx
+++ src/page/AdminWorkflows/Workflows/Workflows.component.tsx
@@ -1,4 +1,11 @@
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { useEffect } from 'react';
 import { useAdminWorkflows } from '../AdminWorkflows.services';
 
@@ -9,9 +16,32 @@ export const Workflows: React.FC = () => {
   }, []);
   return (
     <Box>
-      {workflows.map((workflow) => {
-        return <Box key={workflow.name}>{workflow.name}</Box>;
-      })}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell colSpan={3} align="center">
+                Workflows
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>Name</TableCell>
+              {/* <TableCell>Model</TableCell>
+              <TableCell align="right">Supported</TableCell> */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {workflows.map((workflow) => {
+              return (
+                <TableRow key={workflow.name}>
+                  <TableCell>{workflow.name}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
