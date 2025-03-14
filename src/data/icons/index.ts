db61499760ee4ef087099999907e5bba3493eba6
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-14 18:42:06 +0800

    eslint fix

--- src/data/icons/index.ts
+++ src/data/icons/index.ts
@@ -1,5 +1,6 @@
 import CloseIcon from '@mui/icons-material/Close';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DeleteIcon from '@mui/icons-material/Delete';
 import DoneIcon from '@mui/icons-material/Done';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -7,17 +8,16 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SaveIcon from '@mui/icons-material/Save';
 import XIcon from '@mui/icons-material/X';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 export const Icons = {
   Check: DoneIcon,
   Close: CloseIcon,
   Copy: ContentCopyIcon,
+  Delete: DeleteIcon,
   DownloadFile: FileDownloadIcon,
   GitHub: GitHubIcon,
   LinkedIn: LinkedInIcon,
   Save: SaveIcon,
   Twitter: XIcon,
   YouTube: YouTubeIcon,
-  Delete: DeleteIcon
 };
