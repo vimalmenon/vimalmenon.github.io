778f3b943bdd214b7297d2afe4559152d507059f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-20 19:55:16 +0800

    added nodeType

--- src/page/AdminWorkflowId/Node/Node.service.ts
+++ src/page/AdminWorkflowId/Node/Node.service.ts
@@ -2,11 +2,15 @@ enum fields {
   LLM = 'LLM',
   Prompt = 'Prompt',
   Tools = 'Tools',
+  Input = 'Input',
 }
 
 export const nodeType = (type: string): string[] => {
   if (type === 'agent') {
     return [fields.LLM, fields.Prompt, fields.Tools];
+  }
+  if (type === 'humanInput') {
+    return [fields.Input];
   }
   return [];
 };
