c33f28d4bb8c0d0d1560a2c98eb2032a89089cfb
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-03-25 19:02:29 +0800

    working on MultiSelect

--- src/component/MultiSelect/MultiSelect.component.tsx
+++ src/component/MultiSelect/MultiSelect.component.tsx
@@ -1,3 +1,36 @@
-export const MultiSelect: React.FC = () => {
-  return null;
+'use client';
+
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { IMultiSelect } from './MultiSelect';
+
+export const MultiSelect: React.FC<IMultiSelect> = ({ id, label, onChange, options, value }) => {
+  const handleChange = (event: SelectChangeEvent<string[]>): void => {
+    const {
+      target: { value },
+    } = event;
+    onChange(typeof value === 'string' ? value.split(',') : value);
+  };
+  return (
+    <FormControl size="small" fullWidth>
+      <InputLabel id={id}>{label}</InputLabel>
+      <Select
+        labelId={id}
+        value={value}
+        onChange={handleChange}
+        multiple
+        renderValue={(selected) => selected.join(', ')}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            <Checkbox checked={false} />
+            {option.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 };
--- src/component/MultiSelect/MultiSelect.d.ts
+++ src/component/MultiSelect/MultiSelect.d.ts
@@ -3,4 +3,7 @@ import { IMultiSelectOption } from '@types';
 export interface IMultiSelect {
   options: IMultiSelectOption[];
   value: string[];
+  label: string;
+  id: string;
+  onChange: (value: string[]) => void;
 }
