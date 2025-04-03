22198ab19900d4819a2f4558c475f9d261d987f1
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-04-04 06:41:15 +0800

    improve UI

--- src/page/AdminWorkflowId/Workflow/WorkflowView/WorkflowView.component.tsx
+++ src/page/AdminWorkflowId/Workflow/WorkflowView/WorkflowView.component.tsx
@@ -1,37 +1,48 @@
 'use client';
 
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import { Fragment } from 'react';
 import { Icon } from '@component';
 import { Icons } from '@data';
 import { IWorkflowView } from './WorkflowView';
 
 export const WorkflowView: React.FC<IWorkflowView> = ({ data, onEdit }) => {
   return (
-    <Box sx={{ display: 'flex', flex: '1 1 100%', flexDirection: 'column', gap: 2 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box>Workflow</Box>
-        {!data.complete ? (
-          <Box>
-            <Icon toolTip="Edit" icon={<Icons.Edit />} onClick={onEdit} />
+    <Card>
+      <CardHeader
+        title="Workflow"
+        action={
+          <Fragment>
+            {!data.complete ? <Icon toolTip="Edit" icon={<Icons.Edit />} onClick={onEdit} /> : null}
+          </Fragment>
+        }
+      />
+      <CardContent>
+        <Box sx={{ display: 'flex', flex: '1 1 100%', flexDirection: 'column', gap: 2 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box>Workflow</Box>
           </Box>
-        ) : null}
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box>ID</Box>
-        <Box>{data.id}</Box>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box>Name</Box>
-        <Box>{data.name}</Box>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box>Detail</Box>
-        <Box>{data.detail}</Box>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box>Complete</Box>
-        <Box>{data.complete ? 'True' : 'False'}</Box>
-      </Box>
-    </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box>ID</Box>
+            <Box>{data.id}</Box>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box>Name</Box>
+            <Box>{data.name}</Box>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box>Detail</Box>
+            <Box>{data.detail}</Box>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box>Complete</Box>
+            <Box>{data.complete ? 'True' : 'False'}</Box>
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
   );
 };
