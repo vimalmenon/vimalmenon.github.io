47c6e2222ce1705d1ae4aea9fd628077e79c9e2f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-03 10:50:28 +0800

    Adds loading state to AdminWorkflows
    Introduces a `loading` state to track asynchronous operations in AdminWorkflows. Updates workflow functions to set `loading` appropriately during API calls, improving UI responsiveness and state management.

--- src/page/AdminWorkflows/AdminWorkflows.d.ts
+++ src/page/AdminWorkflows/AdminWorkflows.d.ts
@@ -1,8 +1,10 @@
-import { IWorkflow } from '@types';
+import { IWorkflow, ReactSetState } from '@types';
 
 export interface IUseAdminWorkflows {
   createWorkflow: (name: string) => Promise<void>;
   deleteWorkflow: (id: string) => Promise<void>;
   getWorkflows: () => Promise<void>;
   workflows: IWorkflow[];
+  loading: boolean;
+  setLoading: ReactSetState<boolean>;
 }
--- src/page/AdminWorkflows/AdminWorkflows.services.ts
+++ src/page/AdminWorkflows/AdminWorkflows.services.ts
@@ -8,29 +8,38 @@ import { IUseAdminWorkflows } from './AdminWorkflows';
 
 export const useAdminWorkflows = (): IUseAdminWorkflows => {
   const [workflows, setWorkflows] = useState<IWorkflow[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getWorkflows = async (): Promise<void> => {
+    setLoading(true);
     const { response } = await makeRequest<IGenericResponse<IWorkflow[]>>(APIs.GetWorkflows());
     setWorkflows(response.data);
+    setLoading(false);
   };
 
   const createWorkflow = async (name: string): Promise<void> => {
+    setLoading(true);
     await makeRequest<IGenericResponse<ITool[]>>(
       APIs.CreateWorkflow({
         name,
       })
     );
     await getWorkflows();
+    setLoading(false);
   };
 
   const deleteWorkflow = async (id: string): Promise<void> => {
+    setLoading(true);
     await makeRequest<IGenericResponse<ITool[]>>(APIs.DeleteWorkflow(id));
     await getWorkflows();
+    setLoading(false);
   };
   return {
     createWorkflow,
     deleteWorkflow,
     getWorkflows,
+    loading,
+    setLoading,
     workflows,
   };
 };
