326bc084e66abf92a49cdb42b5d74928e7596b31
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-11 06:31:24 +0800

    Add History component and integrate showHistory functionality in Header

--- src/page/AdminWorkflowId/AdminWorkFlowId.component.tsx
+++ src/page/AdminWorkflowId/AdminWorkFlowId.component.tsx
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { ConfirmDialog } from '@component';
 import { IAdminWorkflowId } from './AdminWorkflowId';
 import { AdminWorkflowIdContext } from './AdminWorkflowId.context';
@@ -17,11 +17,12 @@ import {
   useWorkflowDataHelper,
   useWorkflowFormHelper,
 } from './AdminWorkflowId.service';
+import { History } from './History';
 import { Node } from './Node';
 import { Workflow } from './Workflow';
 
 const Component: React.FC = () => {
-  const { error, selectedNode, setNodeFormMode, workflow } = useWorkflowContext();
+  const { error, selectedNode, setNodeFormMode, showHistory, workflow } = useWorkflowContext();
   const { nodeFormMode, onTabChange, selectedTab } = useTabHelper();
   const {
     createNode,
@@ -59,42 +60,47 @@ const Component: React.FC = () => {
         />
       ) : null}
       {error ? <Alert severity="error">{error}</Alert> : null}
-
-      <Workflow onCancel={viewWorkflowFormMode} data={workflow} />
-      <Divider />
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Tabs value={selectedTab} onChange={onTabChange}>
-          {nodeFormMode === 'CREATE' ? (
-            <Tab label="Create Node" />
-          ) : (
-            nodeTabs.map((node) => <Tab label={node.label} key={node.name} />)
-          )}
-        </Tabs>
-        {nodeFormMode === 'CREATE' ? (
-          <Node
-            mode="CREATE"
-            createNode={createNode}
-            cancelNode={() => setNodeFormMode('UPDATE')}
-          />
-        ) : (
-          nodeTabs.map((node, index) => {
-            if (selectedTab === index && workflow) {
-              return (
-                <Node
-                  data={workflow.nodes[node.name]}
-                  key={node.name}
-                  mode={node.mode}
-                  nodes={getNodeAsList(workflow.nodes)}
-                  deleteNode={() => deleteNode(node.name)}
-                  updateNode={(data) => updateNode(node.name, data)}
-                  setMode={(mode) => setNodeMode(index, mode)}
-                  cancelNode={() => setNodeMode(index, 'VIEW')}
-                />
-              );
-            }
-          })
-        )}
-      </Box>
+      {showHistory ? (
+        <History />
+      ) : (
+        <Fragment>
+          <Workflow onCancel={viewWorkflowFormMode} data={workflow} />
+          <Divider />
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Tabs value={selectedTab} onChange={onTabChange}>
+              {nodeFormMode === 'CREATE' ? (
+                <Tab label="Create Node" />
+              ) : (
+                nodeTabs.map((node) => <Tab label={node.label} key={node.name} />)
+              )}
+            </Tabs>
+            {nodeFormMode === 'CREATE' ? (
+              <Node
+                mode="CREATE"
+                createNode={createNode}
+                cancelNode={() => setNodeFormMode('UPDATE')}
+              />
+            ) : (
+              nodeTabs.map((node, index) => {
+                if (selectedTab === index && workflow) {
+                  return (
+                    <Node
+                      data={workflow.nodes[node.name]}
+                      key={node.name}
+                      mode={node.mode}
+                      nodes={getNodeAsList(workflow.nodes)}
+                      deleteNode={() => deleteNode(node.name)}
+                      updateNode={(data) => updateNode(node.name, data)}
+                      setMode={(mode) => setNodeMode(index, mode)}
+                      cancelNode={() => setNodeMode(index, 'VIEW')}
+                    />
+                  );
+                }
+              })
+            )}
+          </Box>
+        </Fragment>
+      )}
     </Box>
   );
 };
