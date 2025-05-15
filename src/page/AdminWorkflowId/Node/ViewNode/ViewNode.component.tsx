2e4e6677672420402da4b2641fea50f1b02c3df2
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-15 13:07:12 +0800

    Enhance ViewNode component: Integrate workflow context and update 'Next' field rendering logic

--- src/page/AdminWorkflowId/Node/ViewNode/ViewNode.component.tsx
+++ src/page/AdminWorkflowId/Node/ViewNode/ViewNode.component.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Box from '@mui/material/Box';
+import { useWorkflowContext } from '../../AdminWorkflowId.service';
 import { fields, nodeType } from '../Node.service';
 import { IViewNode } from './ViewNode';
 
 export const ViewNode: React.FC<IViewNode> = ({ data }) => {
   const value = nodeType(data.type);
+  const { workflow } = useWorkflowContext();
   return (
     <Box sx={{ display: 'flex', flex: '1 1 100%', flexDirection: 'column', gap: 2 }}>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -46,10 +48,10 @@ export const ViewNode: React.FC<IViewNode> = ({ data }) => {
           <Box>{data.tool}</Box>
         </Box>
       ) : null}
-      {value.includes(fields.Next) ? (
+      {value.includes(fields.Next) && workflow && data.next ? (
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Box>Next</Box>
-          <Box>{data.next}</Box>
+          <Box>{workflow?.nodes[data.next[0] ?? ''].name}</Box>
         </Box>
       ) : null}
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
