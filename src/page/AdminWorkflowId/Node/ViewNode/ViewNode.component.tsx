67c59de49c98607c8cb89386f29db698550cf75c
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-15 13:17:24 +0800

    Refactor ViewNode component: Replace direct access to 'Next' field with a utility function for improved readability

--- src/page/AdminWorkflowId/Node/ViewNode/ViewNode.component.tsx
+++ src/page/AdminWorkflowId/Node/ViewNode/ViewNode.component.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import Box from '@mui/material/Box';
+import { INode } from '@types';
 import { useWorkflowContext } from '../../AdminWorkflowId.service';
 import { fields, nodeType } from '../Node.service';
 import { IViewNode } from './ViewNode';
+
+const convertNextToString = (nodes: Record<string, INode>, next: string[]): string =>
+  next.map((node) => nodes[node].name).join(', ');
 
 export const ViewNode: React.FC<IViewNode> = ({ data }) => {
   const value = nodeType(data.type);
@@ -51,7 +55,7 @@ export const ViewNode: React.FC<IViewNode> = ({ data }) => {
       {value.includes(fields.Next) && workflow && data.next ? (
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Box>Next</Box>
-          <Box>{workflow?.nodes[data.next[0] ?? ''].name}</Box>
+          <Box>{convertNextToString(workflow?.nodes, data.next)}</Box>
         </Box>
       ) : null}
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
