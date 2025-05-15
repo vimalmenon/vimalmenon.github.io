d0809da96010a95d525cdf9d70378b8d92d39922
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-16 07:21:27 +0800

    Update framer-motion to version 12.11.4 and add Transformers link to data

--- src/data/links/index.ts
+++ src/data/links/index.ts
@@ -3,4 +3,8 @@ export const links = [
     link: 'https://manus.im/app/bD1Dr5dDD2w0Xa7ELQhM9y',
     name: 'Manus',
   },
+  {
+    link: 'https://poloclub.github.io/transformer-explainer/',
+    name: 'Transformers',
+  },
 ];
