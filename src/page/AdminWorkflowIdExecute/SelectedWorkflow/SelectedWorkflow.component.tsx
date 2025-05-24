0e1f1018b7182d41f6868a241fe0b4af53fee942
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-24 21:21:45 +0800

    Implement SelectedWorkflow component with Back button functionality

--- src/page/AdminWorkflowIdExecute/SelectedWorkflow/SelectedWorkflow.component.tsx
+++ src/page/AdminWorkflowIdExecute/SelectedWorkflow/SelectedWorkflow.component.tsx
@@ -1,5 +1,13 @@
 'use client';
 
 import Box from '@mui/material/Box';
+import { useWorkflowExecuteHelper } from '../AdminWorkflowIdExecute.service';
 
-export const SelectedWorkflow: React.FC = () => <Box></Box>;
+export const SelectedWorkflow: React.FC = () => {
+  const { setSelectedWorkflow } = useWorkflowExecuteHelper();
+  return (
+    <Box>
+      <button onClick={() => setSelectedWorkflow(null)}>Back</button>
+    </Box>
+  );
+};
