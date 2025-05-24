25e79b110c4e27fa5e37ee82e6d67ec4089aba24
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-05-24 22:10:51 +0800

    Refactor SelectedWorkflow component to include context for selectedWorkflow and display its id and name

--- src/page/AdminWorkflowIdExecute/SelectedWorkflow/SelectedWorkflow.component.tsx
+++ src/page/AdminWorkflowIdExecute/SelectedWorkflow/SelectedWorkflow.component.tsx
@@ -3,15 +3,21 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { Icons } from '@data';
-import { useWorkflowExecuteHelper } from '../AdminWorkflowIdExecute.service';
+import {
+  useAdminWorkflowIdExecuteContext,
+  useWorkflowExecuteHelper,
+} from '../AdminWorkflowIdExecute.service';
 
 export const SelectedWorkflow: React.FC = () => {
   const { setSelectedWorkflow } = useWorkflowExecuteHelper();
+  const { selectedWorkflow } = useAdminWorkflowIdExecuteContext();
   return (
     <Box>
       <Button variant="text" onClick={() => setSelectedWorkflow(null)} startIcon={<Icons.Back />}>
         Back
       </Button>
+      <Box>{selectedWorkflow?.id}</Box>
+      <Box>{selectedWorkflow?.name}</Box>
     </Box>
   );
 };
