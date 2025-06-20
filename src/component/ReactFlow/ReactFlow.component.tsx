22bd901eb5303b8dfcb41479941151cc4273c47b
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-06-20 21:41:32 +0800

    feat: define nodes and edges for ReactFlow component

--- src/component/ReactFlow/ReactFlow.component.tsx
+++ src/component/ReactFlow/ReactFlow.component.tsx
@@ -2,8 +2,22 @@
 import '@xyflow/react/dist/style.css';
 import { Background, Controls, ReactFlow as ReactFlowComponent } from '@xyflow/react';
 
+const nodes = [
+  {
+    data: { label: 'Hello' },
+    id: '1',
+    position: { x: 0, y: 0 },
+  },
+  {
+    data: { label: 'World' },
+    id: '2',
+    position: { x: 0, y: 100 },
+  },
+];
+const edges = [{ id: 'a->b', source: '1', target: '2' }];
+
 export const ReactFlow: React.FC = () => (
-  <ReactFlowComponent>
+  <ReactFlowComponent nodes={nodes} edges={edges} fitView>
     <Background />
     <Controls />
   </ReactFlowComponent>
