6a529797c4aa61eaaf408e3391951998ed64681f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-06-21 20:22:04 +0800

    feat: refactor SelectedWorkflow component to utilize convertWorkflowToView function for rendering ViewData

--- src/page/AdminWorkflowIdExecute/SelectedWorkflow/SelectedWorkflow.component.tsx
+++ src/page/AdminWorkflowIdExecute/SelectedWorkflow/SelectedWorkflow.component.tsx
@@ -3,12 +3,36 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { Fragment } from 'react';
-import { ReactFlow } from '@component';
+import { ReactFlow, ViewData } from '@component';
 import { Icons } from '@data';
+import { IExecuteWorkflow, IViewData } from '@types';
 import {
   useAdminWorkflowIdExecuteContext,
   useWorkflowExecuteHelper,
 } from '../AdminWorkflowIdExecute.service';
+
+const convertWorkflowToView = (data: IExecuteWorkflow): IViewData[] => {
+  const result: IViewData[] = [];
+  if (data.id) {
+    result.push({
+      label: 'ID',
+      value: data.id,
+    });
+  }
+  if (data.name) {
+    result.push({
+      label: 'Name',
+      value: data.name,
+    });
+  }
+  if (data.status) {
+    result.push({
+      label: 'Status',
+      value: data.status,
+    });
+  }
+  return result;
+};
 
 export const SelectedWorkflow: React.FC = () => {
   const { setSelectedWorkflow } = useWorkflowExecuteHelper();
@@ -20,11 +44,12 @@ export const SelectedWorkflow: React.FC = () => {
       </Button>
       {selectedWorkflow ? (
         <Fragment>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <ViewData data={convertWorkflowToView(selectedWorkflow)} />
+          {/* <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
             <Box>{selectedWorkflow.id}</Box>
             <Box>{selectedWorkflow.name}</Box>
             <Box>{selectedWorkflow.status}</Box>
-          </Box>
+          </Box> */}
           <Box>
             <div style={{ display: 'flex', flex: '1 1 100%', height: '600px' }}>
               <ReactFlow nodes={[]} />
