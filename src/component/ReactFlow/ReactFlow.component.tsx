f90d290eb495f6fcab1a31d1c7a63082643127ed
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-06-25 06:11:44 +0800

    feat: add colorMode prop to ReactFlow component for improved theming

--- src/component/ReactFlow/ReactFlow.component.tsx
+++ src/component/ReactFlow/ReactFlow.component.tsx
@@ -14,6 +14,7 @@ export const ReactFlow: React.FC<IReactFlow> = ({ edges, nodes }) => (
     fitView
     nodesConnectable={false}
     nodesDraggable={false}
+    colorMode="light"
   >
     <Background />
     <Controls />
