d11b5c17cd5b7cc2782b204be78736ef1e41840a
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-06-29 23:34:03 +0800

    fix: simplify Cancel button in NodeForm and ExecuteForm by removing commented code

--- src/page/AdminWorkflowId/Common/NodeForm/NodeForm.component.tsx
+++ src/page/AdminWorkflowId/Common/NodeForm/NodeForm.component.tsx
@@ -243,14 +243,7 @@ export const NodeForm: React.FC<INodeForm> = ({
       ) : null}
 
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Button
-          variant="outlined"
-          onClick={onCancel}
-          endIcon={<Icons.Close />}
-          disabled={loading}
-          // loading={loading}
-          // loadingPosition="end"
-        >
+        <Button variant="outlined" onClick={onCancel} endIcon={<Icons.Close />} disabled={loading}>
           Cancel
         </Button>
         {mode === 'UPDATE' && updateNode ? (
