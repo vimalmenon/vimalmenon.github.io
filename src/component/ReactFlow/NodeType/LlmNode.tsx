c44f8a43d3c1b6b0a0089406e581155c54d9e32d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-04 07:55:47 +0800

    fix: refactor LlmNode component for improved readability and structure

--- src/component/ReactFlow/NodeType/LlmNode.tsx
+++ src/component/ReactFlow/NodeType/LlmNode.tsx
@@ -7,45 +7,43 @@ import Typography from '@mui/material/Typography';
 import { Handle, Position } from '@xyflow/react';
 import { INodeType } from './NodeType';
 
-export const LlmNode: React.FC<INodeType> = ({ data }) => {
-  return (
+export const LlmNode: React.FC<INodeType> = ({ data }) => (
+  <Box
+    className="text-updater-node"
+    component={Paper}
+    minWidth={'300px'}
+    sx={{ borderRadius: '5px', padding: '10px' }}
+  >
     <Box
-      className="text-updater-node"
-      component={Paper}
-      minWidth={'300px'}
-      sx={{ borderRadius: '5px', padding: '10px' }}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+      }}
     >
+      {' '}
       <Box
         sx={{
+          alignItems: 'center',
+          borderRadius: '5px',
           display: 'flex',
-          flexDirection: 'column',
-          gap: 1,
+          justifyContent: 'space-between',
+          padding: 1,
         }}
       >
-        {' '}
-        <Box
-          sx={{
-            alignItems: 'center',
-            borderRadius: '5px',
-            display: 'flex',
-            justifyContent: 'space-between',
-            padding: 1,
-          }}
-        >
-          <span>
-            {data.label} ({data.type})
-          </span>
-        </Box>
-        <Divider />
-        <Box sx={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
-          <Box sx={{ display: 'flex', flex: 1, justifyContent: 'space-between' }}>
-            <Typography sx={{ fontWeight: 'bold' }}>Prompt</Typography>
-            <span>{data.node.prompt}</span>
-          </Box>
-        </Box>
-        <Handle type="target" position={Position.Top} />
-        <Handle type="source" position={Position.Bottom} />
+        <span>
+          {data.label} ({data.type})
+        </span>
       </Box>
+      <Divider />
+      <Box sx={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
+        <Box sx={{ display: 'flex', flex: 1, justifyContent: 'space-between' }}>
+          <Typography sx={{ fontWeight: 'bold' }}>Prompt</Typography>
+          <span>{data.node.prompt}</span>
+        </Box>
+      </Box>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
     </Box>
-  );
-};
+  </Box>
+);
