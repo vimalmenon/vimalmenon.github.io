abad796489dc2ad0537b6d3f63d5e48e7b5b2143
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-07-05 18:33:05 +0800

    feat: refactor ExecuteNode to use BaseNode component for improved structure

--- src/component/ReactFlow/NodeType/ExecuteNode.tsx
+++ src/component/ReactFlow/NodeType/ExecuteNode.tsx
@@ -1,35 +1,11 @@
 'use client';
 
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { Handle, Position } from '@xyflow/react';
-import { Icon } from '@component';
-import { Icons } from '@data';
+import { BaseNode } from './BaseNode';
 import { INodeType } from './NodeType';
 
 export const ExecuteNode: React.FC<INodeType> = ({ data }) => (
-  <Box
-    className="text-updater-node"
-    component={Paper}
-    minWidth={'400px'}
-    sx={{ borderRadius: '5px', padding: '10px' }}
-  >
-    <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between' }}>
-      <span>
-        {data.label} ({data.type})
-      </span>
-
-      <Icon
-        toolTip="Execute"
-        icon={<Icons.Play />}
-        onClick={() =>
-          data.onExecute({
-            id: data.id,
-          })
-        }
-      />
-    </Box>
-    <Handle type="target" position={Position.Top} />
-    <Handle type="source" position={Position.Bottom} />
-  </Box>
+  <BaseNode data={data} onExecute={data.onExecute} disableExecute={false}>
+    <Box></Box>
+  </BaseNode>
 );
